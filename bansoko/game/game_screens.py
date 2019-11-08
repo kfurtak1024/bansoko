7c966a34e67478d1c7d70f135620ab68a1563096
Krzysztof Furtak <kfurtak1024@gmail.com>
2019-11-08 19:40:09 +0100

    Introduce draft, wip resource file; Move screen to graphics package; Group screens in game screens module
    Signed-off-by: Krzysztof Furtak <kfurtak1024@gmail.com>

---
+++ bansoko/game/game_screens.py
@@ -0,0 +1,70 @@
+import pyxel
+
+from bansoko.graphics.screen import Screen
+
+
+class MainMenuScreen(Screen):
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_1):
+            return GameScreen()
+        if pyxel.btnp(pyxel.KEY_2):
+            return ChooseLevelScreen()
+        if pyxel.btnp(pyxel.KEY_3):
+            return OptionsScreen()
+        if pyxel.btnp(pyxel.KEY_4):
+            return None
+        return self
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(16, 16, "MAIN MENU\n=========\n1) Start Game\n2) Choose Level\n3) Options\n4) Exit", 1)
+
+
+class GameScreen(Screen):
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_1):
+            return GamePausedScreen()
+        return self
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(16, 16, "PLAYING GAME\n============\n1) Pause Menu", 2)
+
+
+class GamePausedScreen(Screen):
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_1):
+            return None
+        if pyxel.btnp(pyxel.KEY_2):
+            return OptionsScreen()
+        if pyxel.btnp(pyxel.KEY_3):
+            return MainMenuScreen()
+        return self
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(16, 16, "GAME PAUSED\n===========\n1) Continue Game\n2) Options\n3) Exit To Main Menu", 3)
+
+
+class ChooseLevelScreen(Screen):
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_1):
+            return GameScreen()
+        if pyxel.btnp(pyxel.KEY_2):
+            return None
+        return self
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(16, 16, "CHOOSE LEVEL\n============\n1) Start Level\n2) Exit To Main Menu", 4)
+
+
+class OptionsScreen(Screen):
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_1):
+            return None
+        return self
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(16, 16, "OPTIONS\n============\n1) Back", 4)
