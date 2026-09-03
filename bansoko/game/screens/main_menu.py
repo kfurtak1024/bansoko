9e003abaefb6761d36bb67aa494c0175f3509789
Krzysztof Furtak <kfurtak1024@gmail.com>
2026-09-03 08:38:09 +0200

    Update copyright year in main menu

--- bansoko/game/screens/main_menu.py
+++ bansoko/game/screens/main_menu.py
@@ -36,7 +36,7 @@ class MainMenuController(MenuController):
     def draw(self, draw_as_secondary: bool = False) -> None:
         super().draw(draw_as_secondary)
         draw_text(
-            Point(79, 240), "(c) 2020-2022 KRZYSZTOF FURTAK", TextStyle(color=7, shadow_color=1))
+            Point(79, 240), "(c) 2020-2026 KRZYSZTOF FURTAK", TextStyle(color=7, shadow_color=1))
         draw_text(
             Point(11, 240), f"v{__version__}", TextStyle(color=1))
 
